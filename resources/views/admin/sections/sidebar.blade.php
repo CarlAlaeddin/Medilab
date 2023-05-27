3bfb362c22e784976a9c0acef4c1c560efa29a5f
Carl Alaeddin <132349884+CarlAlaeddin@users.noreply.github.com>
2023-05-27 18:57:51 +0330

    fixed route

--- resources/views/admin/sections/sidebar.blade.php
+++ resources/views/admin/sections/sidebar.blade.php
@@ -36,7 +36,7 @@
         <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
             <div class="bg-white py-2 collapse-inner rounded">
                 <h6 class="collapse-header">Page :</h6>
-                <a class="collapse-item" href="">Hero Section</a>
+                <a class="collapse-item" href="{{ route('admin.setting.edit') }}">Page Setting</a>
                 <a class="collapse-item" href="">Why us</a>`
             </div>
         </div>
