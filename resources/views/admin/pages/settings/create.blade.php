6776cf08434f9ec675609b5171c8717caba7a88a
Carl Alaeddin <132349884+CarlAlaeddin@users.noreply.github.com>
2023-05-27 17:51:46 +0330

    fixed create view

---
+++ resources/views/admin/pages/settings/create.blade.php
@@ -0,0 +1,20 @@
+@extends('admin.layouts.master')
+@section('title')
+{{ __('update') }}
+@endsection
+
+@section('styles')
+
+@endsection
+
+@section('scripts')
+
+@endsection
+
+@section('content')
+<!-- Content Row -->
+<div class="row">
+
+</div>
+
+@endsection
