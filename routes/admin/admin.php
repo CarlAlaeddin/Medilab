6779710828c2bf7f3e71c365607fbcae0bc2db0f
Carl Alaeddin <132349884+CarlAlaeddin@users.noreply.github.com>
2023-05-27 17:03:36 +0330

    separating routes

---
+++ routes/admin/admin.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Http\Controllers\SettingController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes Admin
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::prefix('/admin-panel')
+    ->name('admin.')
+    ->group(function () {
+        #________ This root belongs to the settings site
+        Route::prefix('/settings')
+            ->name('setting.')
+            ->controller(SettingController::class)
+            ->group(function () {
+                Route::get('/', 'index')->name('index');
+            });
+    });
--- routes/home/home.php
+++ routes/home/home.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
-| Web Routes
+| Web Routes Home
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
@@ -12,3 +12,8 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+
+
+Route::get('/', function () {
+    return view('home.index');
+});
