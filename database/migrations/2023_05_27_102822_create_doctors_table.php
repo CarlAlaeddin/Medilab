1ba028960cd73ad331f1cf835bf9cf17cb8ebff5
Carl Alaeddin <132349884+CarlAlaeddin@users.noreply.github.com>
2023-05-28 13:41:22 +0330

    fixed doctors table and adde new cloumn

--- database/migrations/2023_05_27_102822_create_doctors_table.php
+++ database/migrations/2023_05_27_102822_create_doctors_table.php
@@ -14,18 +14,20 @@ return new class extends Migration
         Schema::create('doctors', function (Blueprint $table) {
             $table->id();
 
+            $table->foreignId('position_id')->unsigned();
             $table->string('image');
             $table->string('name');
-            $table->string('position');
             $table->text('biography');
-
+            $table->boolean('is_active');
             $table->string('linkedin')->nullable();
             $table->string('twitter')->nullable();
             $table->string('facebook')->nullable();
             $table->string('instagram')->nullable();
 
-
             $table->timestamps();
+
+            $table->foreign('position_id')->references('id')->on('positions')->onDelete('cascade');
+
         });
     }
 
