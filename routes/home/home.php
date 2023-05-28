598d9797bdc28bd8d4cd84067b25d9c7588ec217
Carl Alaeddin <132349884+CarlAlaeddin@users.noreply.github.com>
2023-05-28 07:11:17 +0330

    added name space admin for setting controller

--- routes/home/home.php
+++ routes/home/home.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Home\MedicalController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +15,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Route::get('/', function () {
-    return view('home.index');
-});
+Route::get('/',[MedicalController::class, 'index'])->name('index');
