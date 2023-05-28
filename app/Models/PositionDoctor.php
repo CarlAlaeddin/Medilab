3e3ace5dbcbf44897068a3ccc8491426531e7347
Carl Alaeddin <132349884+CarlAlaeddin@users.noreply.github.com>
2023-05-28 15:26:28 +0330

    fixed model

--- app/Models/PositionDoctor.php
+++ app/Models/PositionDoctor.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class PositionDoctor extends Model
 {
     use HasFactory;
+
+    protected $table = 'position_doctor';
+
+    protected $guarded = [];
+
+    /**
+     * Summary of getIsActiveAttribute
+     * @param mixed $is_active
+     * @return string
+     */
+    public function getIsActiveAttribute($is_active): string
+    {
+        return $is_active ? 'Active' : 'DeActive';
+    }
 }
