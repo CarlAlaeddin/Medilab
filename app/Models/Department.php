873efd216ceb7f815222e656f510dc0773b61f4b
Carl Alaeddin <132349884+CarlAlaeddin@users.noreply.github.com>
2023-05-28 12:27:14 +0330

    fixed get attribute is active

--- app/Models/Department.php
+++ app/Models/Department.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Department extends Model
 {
     use HasFactory;
+
+    protected $table = 'departments';
+
+    protected $guarded = [];
+
+    /**
+     * Summary of getIsActiveAttribute
+     * @param mixed $is_active
+     * @return string
+     */
+    public function getIsActiveAttribute($is_active): string
+    {
+        return $is_active ? 'Active' : 'DeActive';
+    }
 }
