f35c3d33a2b445125e15511658f8e90ea8b2d180
Carl Alaeddin <132349884+CarlAlaeddin@users.noreply.github.com>
2023-05-28 10:10:29 +0330

    fixed model why choose

---
+++ app/Models/WhyChoose.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class WhyChoose extends Model
+{
+    use HasFactory;
+
+    protected $table = 'why_choose';
+
+    protected $guarded = [];
+
+        /**
+     * Summary of getIsActiveAttribute
+     * @param mixed $is_active
+     * @return string
+     */
+    public function getIsActiveAttribute($is_active): string
+    {
+        return $is_active ? 'Active' : 'DeActive';
+    }
+}
