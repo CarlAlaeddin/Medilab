e18cf93eb5790e85d7bc8ea6fef8ad4b77ef6069
Carl Alaeddin <132349884+CarlAlaeddin@users.noreply.github.com>
2023-05-28 12:58:45 +0330

    fixed all methods

--- app/Http/Controllers/Admin/DepartmentController.php
+++ app/Http/Controllers/Admin/DepartmentController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Models\Department;
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Requests\DepartmentRequest;
+use RealRashid\SweetAlert\Facades\Alert;
 
 class DepartmentController extends Controller
 {
@@ -12,7 +14,8 @@ class DepartmentController extends Controller
      */
     public function index()
     {
-        //
+        $departments = Department::query()->orderBy('id', 'DESC')->paginate(10);
+        return view('admin.pages.department.index', compact('departments'));
     }
 
     /**
@@ -20,46 +23,98 @@ class DepartmentController extends Controller
      */
     public function create()
     {
-        //
+        return view('admin.pages.department.create');
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(DepartmentRequest $request)
     {
-        //
+        #_____________ upload image
+
+        $imageFile = $request->file('image');
+        $imageFileName = generateFileName($request->image->getClientOriginalExtension());
+
+
+        #_______________________________ Create data
+        Department::create([
+            'image'        =>   $imageFileName,
+            'name'        =>   $request->input('name'),
+            'description'  =>   $request->input('description'),
+            'is_active'    =>   $request->input('is_active'),
+        ]);
+
+        $imageFile->move(public_path('upload/department/'), $imageFileName);
+
+        #___________________________________ Alert success
+        Alert::success('success report', 'created new department successfully');
+
+        #___________________________________ Redirect to index method
+        return redirect()->route('admin.department.index');
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Department $department)
     {
-        //
+        return view('admin.pages.department.show', compact('department'));
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(Department $department)
     {
-        //
+        return view('admin.pages.department.edit', compact('department'));
     }
+
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(DepartmentRequest $request, Department $department)
     {
-        //
+        #_____________ upload and unlink why_us_image
+        if (!is_null($request->image)) {
+
+            if (file_exists(public_path('/upload/department/' . $department->image))) {
+                unlink(public_path('upload/department/' . $department->image));
+            }
+
+            $imageFile = $request->file('image');
+            $imageFileName = generateFileName($request->image->getClientOriginalExtension());
+            $department->image = $imageFileName;
+
+            $imageFile->move(public_path('upload/department/'), $imageFileName);
+        }
+        #_______________________________ Create data
+        $department->update([
+            'name'        =>   $request->input('name'),
+            'description'  =>   $request->input('description'),
+            'is_active'    =>   $request->input('is_active'),
+        ]);
+
+        #___________________________________ Alert success
+        Alert::success('success report', 'updated department successfully');
+
+        #___________________________________ updated
+        $department->update();
+
+        #___________________________________ Redirect to index method
+        return redirect()->route('admin.department.index');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Department $department)
     {
-        //
+        $department->delete();
+        #___________________________________ Alert success
+        Alert::warning('success report', 'deleted department successfully');
+        #___________________________________ Redirect to index method
+        return redirect()->route('admin.department.index');
     }
 }
