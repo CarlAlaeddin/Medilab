c8c76483382e372bb5f929c07f7646eaca1106cd
Carl Alaeddin <132349884+CarlAlaeddin@users.noreply.github.com>
2023-05-29 10:06:27 +0330

    fixed model gallery

---
+++ app/Models/Gallery.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Gallery extends Model
+{
+    use HasFactory;
+
+
+    protected $table = "galleries";
+    protected $fillable = ['image', 'is_active'];
+
+    /**
+     * Summary of getIsActiveAttribute
+     * @param mixed $is_active
+     * @return string
+     */
+    public function getIsActiveAttribute($is_active): string
+    {
+        return $is_active ? 'Active' : 'DeActive';
+    }
+}
