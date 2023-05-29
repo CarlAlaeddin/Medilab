c36e246cbdcc2085812f9618b94ac51ed9a35c1b
Carl Alaeddin <132349884+CarlAlaeddin@users.noreply.github.com>
2023-05-29 11:04:08 +0330

    fixed contact home store method

---
+++ app/Http/Controllers/Home/ContactController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\Home;
+
+use App\Models\Contact;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\ContactRequest;
+use RealRashid\SweetAlert\Facades\Alert;
+
+class ContactController extends Controller
+{
+    public function store(ContactRequest $contactRequest)
+    {
+        Contact::create([
+            'name'      =>  $contactRequest->input('name'),
+            'email'     =>  $contactRequest->input('email'),
+            'subject'   =>  $contactRequest->input('subject'),
+            'message'   =>  $contactRequest->input('message'),
+        ]);
+
+        #___________________________________ Alert success
+        Alert::success('success report', 'Created new message successfully');
+
+        #___________________________________ Redirect to index method
+        return redirect()->back();
+    }
+}
