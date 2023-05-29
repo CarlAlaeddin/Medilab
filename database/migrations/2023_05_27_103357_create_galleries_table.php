89bff49cbb533e1d2dc2c35b5f2683e13b665f8c
Carl Alaeddin <132349884+CarlAlaeddin@users.noreply.github.com>
2023-05-29 10:04:53 +0330

    add new cloumn is active for database galleries

--- database/migrations/2023_05_27_103357_create_galleries_table.php
+++ database/migrations/2023_05_27_103357_create_galleries_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
 
             $table->string('image');
+            $table->boolean('is_active');
 
             $table->timestamps();
         });
