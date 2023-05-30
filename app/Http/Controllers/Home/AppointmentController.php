01a79945d41aeb6d5fa814ff2bc30feff00455b6
Carl Alaeddin <132349884+CarlAlaeddin@users.noreply.github.com>
2023-05-30 10:21:27 +0330

    fixig notification

--- app/Http/Controllers/Home/AppointmentController.php
+++ app/Http/Controllers/Home/AppointmentController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers\Home;
 
 use App\Models\Appointment;
 use App\Http\Controllers\Controller;
+use App\Models\User;
+use App\Notifications\Admin\AppointmentNotification;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Notification;
 use RealRashid\SweetAlert\Facades\Alert;
 use App\Http\Requests\AppointmentRequest;
 
@@ -11,7 +15,7 @@ class AppointmentController extends Controller
 {
     public function store(AppointmentRequest $appointmentRequest)
     {
-        Appointment::create([
+        $appointment =  new Appointment([
             'department_id'  => $appointmentRequest->input('department_id'),
             'doctor_id'      => $appointmentRequest->input('doctor_id'),
             'name'           => $appointmentRequest->input('name'),
@@ -25,6 +29,13 @@ class AppointmentController extends Controller
         #___________________________________ Alert success
         Alert::success('success report', 'Your request has been registered and will be answered after checking');
 
+        #__________________________________ Notification
+
+        $thisUser = Auth::user();
+
+        Notification::send($thisUser, new AppointmentNotification($appointment));
+
+        $appointment->save();
         #___________________________________ Redirect to index method
         return redirect()->back();
     }
